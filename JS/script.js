af881019d451539976094e817a4052d5d6ce22dc
Vinit Solanki <121821691+vinit-solanki@users.noreply.github.com>
2024-06-19 14:05:53 +0530

    "changes"

--- JS/script.js
+++ JS/script.js
@@ -156,7 +156,7 @@ s.replace("existing","replacing")
 // });
 
 // // map vs forEach
-nums=[1,2,3,4,5,6]
+// nums=[1,2,3,4,5,6]
 // const op1=nums.forEach(function(ele,idx,nums){
 //     console.log(ele*2);
 // });
@@ -165,40 +165,77 @@ nums=[1,2,3,4,5,6]
 // });
 // console.log(op2);
 
-nums.splice(0,0,0)
-console.log(nums)
+// nums.splice(0,0,0)
+// console.log(nums)
 
-nums.splice(nums.length,0,7)
-console.log(nums);  
+// nums.splice(nums.length,0,7)
+// console.log(nums);  
 
-const mnths=["Jan","Mar","Apr",
-    "June","July"
-];
-// mnths.push("Dec");
-mnths.splice(mnths.length,0,"Nov","Dec")
-const idx0=mnths.indexOf("Jan")
-mnths.splice(idx0,1,"January")
-const idx1=mnths.indexOf("Mar")
-mnths.splice(idx1,0,"February")
-console.log(mnths);
+// const mnths=["Jan","Mar","Apr",
+//     "June","July"
+// ];
+// // mnths.push("Dec");
+// mnths.splice(mnths.length,0,"Nov","Dec")
+// const idx0=mnths.indexOf("Jan")
+// mnths.splice(idx0,1,"January")
+// const idx1=mnths.indexOf("Mar")
+// mnths.splice(idx1,0,"February")
+// console.log(mnths);
 
-nums.push(0);
-console.log(nums);
+// nums.push(0);
+// console.log(nums);
 
-let updatednums=nums.filter((ele)=>{
-    return ele === 0;
+// let updatednums=nums.filter((ele)=>{
+//     return ele === 0;
+// });
+// console.log(updatednums);
+
+// const products=[
+//     {name:"FaceWash",price:120},
+//     {name:"Phone-Case",price:145},
+//     {name:"Chain",price:46},
+//     {name:"Tie",price:60},
+// ];
+// // Products greater then 50 and less than 120 
+// const flteProduct=products.filter((ele)=>{
+//     if(ele.price>=50&&ele.price<=120)
+//     return ele.name
+// });
+// console.log(flteProduct);
+
+// products.sort((a,b)=>a.price-b.price);
+// console.log(products);
+
+// // sq of even no.s
+// const evensqs=nums.map(
+//     (ele)=>{return ele%2===0?ele*ele:undefined}
+// ).filter((currEle)=>currEle===!undefined);
+// console.log(evensqs);
+
+// for(let i=0;i<=4;i++){
+//     let row='';
+//     for(let j=0;j<=i;j++){
+//         row+='*';
+//     }
+//     console.log(row);
+// }
+
+// Strings:
+let naam=Array.from("Vinit Solanki");
+let naamMap=naam.map((curr,idx)=>{
+    return `${curr} - ${idx}`;
 });
-console.log(updatednums);
-
-const products=[
-    {name:"FaceWash",price:120},
-    {name:"Phone-Case",price:145},
-    {name:"Chain",price:46},
-    {name:"Tie",price:60},
-];
-// Products greater then 50 and less than 120 
-const flteProduct=products.filter((ele)=>{
-    if(ele.price>=50&&ele.price<=120)
-    return ele.name
-});
-console.log(flteProduct);
+console.log(naamMap);
+names=["John","Kane","Leean"]
+let mr=names.map((names)=>`Mr. ${names}`);
+console.log(names);
+console.log(mr);
+// String methods:
+/*
+.indexOf(searchString,position)
+.lastIndexOf(searchString,position)
+.search(regexp)
+.slice(start,end)
+.charAt(index from 0 or Last indexing)
+.at()->returns the character at specified index
+*/
