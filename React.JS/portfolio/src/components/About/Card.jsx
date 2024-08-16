2a7fa0957ad55704db42633e68e78c2836963c67
Vinit Solanki <2022.vinit.solanki@ves.ac.in>
2024-08-17 04:48:23 +0530

    update-2

--- React.JS/portfolio/src/components/About/Card.jsx
+++ React.JS/portfolio/src/components/About/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 function Card({source}) {
-let path =`/assets/${source}.png`;
+let path =`src/assets/${source}.png`;
   return (
 <div id='card' className="p-1 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <img src={path} alt="" />
